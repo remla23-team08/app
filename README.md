42a92598c3c68972dbd97c18a5cdb5dfc89ab76c
Janaina Moreira-Kanaley <J.M.Moreira-Kanaley@student.tudelft.nl>
2023-05-08 10:07:33 +0200

    Add base frontend project files

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# app
+# App
+
+### Requirements
+- NodeJS
+- NPM
+
+### Installing dependencies
+`npm install`
+
+### Building
+`npm run build`
+
+### Running
+`npm run start`
+
+### Formatting code
+`npm run format`
+
 
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const PORT = 8083;
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: PORT,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: "App",
+      metaDesc: "App",
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+};
