83fa1379b6b07ab5be9b5e7136d02e09386ede04
Janaina Moreira-Kanaley <J.M.Moreira-Kanaley@student.tudelft.nl>
2023-05-08 22:45:04 +0200

    Change JSON format

--- src/index.ts
+++ src/index.ts
@@ -5,32 +5,32 @@ const goodReview: string = "I'm glad to hear you had a good experience.";
 
 btn.addEventListener("click", () => {
   let review = (<HTMLInputElement>document.getElementById("review")).value;
-  sendData({ submission: review });
+  sendData({ review: review });
 });
 
 function sendData(data) {
   const XHR = new XMLHttpRequest();
-  const FD = new FormData();
-
-  // Push our data into our FormData object
-  FD.append("review", data.submission);
 
   XHR.onreadystatechange = function () {
     if (XHR.readyState == XMLHttpRequest.DONE && XHR.status == 200) {
       console.log("Successful Request");
       console.log(XHR.responseText);
-    } else {
-      if (Math.random() > 0.5) {
+      let prediction = JSON.parse(XHR.responseText).prediction;
+      if (prediction == 0) {
         reqResult.textContent = badReview;
       } else {
         reqResult.textContent = goodReview;
       }
+    } else {
+      reqResult.textContent = "Error occurred.";
     }
   };
 
   // Set up our request
   XHR.open("POST", process.env.MODEL_SERVICE_URL);
 
-  // Send our FormData object; HTTP headers are set automatically
-  XHR.send(FD);
+  XHR.setRequestHeader("Content-type", "application/json");
+
+  // Send our object; HTTP headers are set automatically
+  XHR.send(JSON.stringify(data));
 }
