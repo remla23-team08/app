c5b588be770eb24dd52f4fa3920fe8a296fd5ff9
gadmiraal <gijsadmiraal@hotmail.com>
2023-05-15 22:31:54 +0200

    Added a form and bootstrap

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,47 @@
-const btn = document.getElementById("submit");
+const form = document.getElementById("reviewForm")
+form.addEventListener("submit", onSubmit)
 const reqResult = document.getElementById("requestResult");
-const badReview: string = "I'm sorry to hear you had a bad experience.";
-const goodReview: string = "I'm glad to hear you had a good experience.";
+const afterSubmit = document.getElementById("afterSubmit");
+const badReview: string = "We are sorry to hear you had a bad experience. 🙁";
+const goodReview: string = "We are glad to hear you had a good experience. 😀";
 
-btn.addEventListener("click", () => {
-  let review = (<HTMLInputElement>document.getElementById("review")).value;
-  sendData({ review: review });
-});
+function onSubmit(event) {
+  event.preventDefault()
+  console.log(event)
+  if(checkForms()){ 
+    let review = (<HTMLInputElement>document.getElementById("reviewField")).value;
+    sendData({ review: review });
+    afterSubmit.style.visibility = "visible"
+  }
+}
+
+function checkForms(): boolean {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.getElementById('reviewForm')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+          return false
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+  return true
+}
 
 function sendData(data) {
   const XHR = new XMLHttpRequest();
 
   XHR.onreadystatechange = function () {
+    console.log(XHR);
     if (XHR.readyState == XMLHttpRequest.DONE && XHR.status == 200) {
       console.log("Successful Request");
       console.log(XHR.responseText);
