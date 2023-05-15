03716a91b1eabc557b3d3423386aa8bb8b4f55e5
gadmiraal <gijsadmiraal@hotmail.com>
2023-05-15 22:32:16 +0200

    Added a home page

---
+++ src/home.ts
@@ -0,0 +1,23 @@
+// let maxWidth = 0;
+
+// let buttons = document.getElementsByClassName('btn')
+
+// console.log(buttons)
+
+// Array.prototype.slice.call(buttons)
+// 	.forEach(function (button) {
+// 		const width = button.style.width;
+// 		console.log(width)
+// 		if(width > maxWidth) {maxWidth = width}
+// 	}
+// )
+
+// console.log(maxWidth)
+
+// Array.prototype.slice.call(buttons)
+// 	.forEach(function (button) {
+// 		button.style.width =  maxWidth +'px'
+// 	}
+// )
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
+const Dotenv = require("dotenv-webpack")
 const PORT = 8083;
+const pages = ['index', 'home']
 
 module.exports = {
-  entry: "./src/index.ts",
+  entry: {
+    index: "./src/index.ts",
+    home: "./src/home.ts",
+  },
   output: {
-    filename: "main.js",
+    filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
   },
   module: {
@@ -17,10 +21,16 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: /\.css$/i,
+        test: /\\.css$/,
         use: [
-          { loader: "style-loader" },
-          { loader: "css-loader", options: { url: false } },
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
         ],
       },
     ],
@@ -32,15 +42,18 @@ module.exports = {
     compress: true,
     port: PORT,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      hash: true,
-      title: "App",
-      metaDesc: "App",
-      template: "./src/index.html",
-      filename: "index.html",
-      inject: "body",
-    }),
-    new Dotenv(),
-  ],
+  plugins: [new Dotenv()].concat(
+    pages.map(
+      (page) =>
+        new HtmlWebpackPlugin({
+          hash: true,
+          title: "App",
+          myPageHeader: page,
+          metaDesc: "App",
+          template: `./src/${page}.html`,
+          filename: `${page}.html`,
+          chunks: [page]
+        })
+    ),
+  ),
 };
